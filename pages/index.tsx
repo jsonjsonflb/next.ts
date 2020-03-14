421b868c8761237dfd4a2351be80e1c54eb976e8
jsonjosn <jsonjosnflb@gmail.com>
2020-03-14 16:21:40 +0800

    引入rudux

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,38 @@
-import { NextPage } from 'next';
-import Layout from '../components/MyLayout';
-import { Provider } from 'react-redux';
-import withRedux from 'next-redux-wrapper';
-import store from '../redux/store';
+import React from 'react';
+import { connect } from 'react-redux';
+import {
+  decrementCounter,
+  incrementCounter
+} from '../redux/actions/counterActions';
 
-const Home: NextPage = (props: any) => {
-  const { Component, pageProps, store } = props;
+class App extends React.Component {
+  static getInitialProps({ store }: { store: any }) {}
 
-  return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
-  );
+  constructor(props: any) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={(this.props as any).incrementCounter}>
+          Increment
+        </button>
+        <button onClick={(this.props as any).decrementCounter}>
+          Decrement
+        </button>
+        <h1>{(this.props as any).counter}</h1>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state: any) => ({
+  counter: state.counter.value
+});
+
+const mapDispatchToProps = {
+  incrementCounter: incrementCounter,
+  decrementCounter: decrementCounter
 };
-
-Home.getInitialProps = async function(context: any) {
-  const { Component, ctx } = context;
-  const pageProps = Component.getInitialProps
-    ? await Component.getInitialProps(ctx)
-    : {};
-
-  //Anything returned here can be accessed by the client
-  return { pageProps };
-};
-
-export default Home;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
